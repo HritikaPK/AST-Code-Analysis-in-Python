a34642e66bcd31b3b2007c78f46d9409363cc578
Hritika Kalghatgi <hkalghatgi@uwaterloo.ca>
2024-10-06 19:45:43 -0400

    Update run_analysis1.sh to separate true positives and true negatives printing

--- run_analysis1.sh
+++ run_analysis1.sh
@@ -1,11 +1,33 @@
 #!/bin/bash
 
 # Running 1.py on test cases from true_positives_1 and true_negative_1
-echo "Running 1.py on true_positives_1 and true_negative_1"
-for testcase in true_positives_1/* true_negative_1/*; do
+echo "Running 1.py on true_positives_1"
+echo
+echo
+for testcase in true_positives_1/*; do
+  echo "-------------------------"
   echo "Running 1.py with test case: $testcase"
+  echo
   python 1.py "$testcase"
   echo "Finished running 1.py with test case: $testcase"
   echo "-------------------------"
+  echo
+  echo
+done
+echo
+echo
+echo
+echo "Running 1.py on true_negative_1"
+echo
+echo
+for testcase in true_negative_1/*; do
+  echo "-------------------------"
+  echo "Running 1.py with test case: $testcase"
+  echo
+  python 1.py "$testcase"
+  echo "Finished running 1.py with test case: $testcase"
+  echo "-------------------------"
+  echo
+  echo
 done
 
