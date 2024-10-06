ea6abe74c98abe4e1ea43c4677c8af1e00c68e4f
Hritika Kalghatgi <hkalghatgi@uwaterloo.ca>
2024-10-06 19:45:53 -0400

    Update run_analysis2.sh with some spacing

--- run_analysis2.sh
+++ run_analysis2.sh
@@ -2,9 +2,15 @@
 
 # Running 2.py on test cases from true_positives_2
 echo "Running 2.py on true_positives_2"
+echo
+echo
 for testcase in true_positives_2/*; do
+  echo "-------------------------"
   echo "Running 2.py with test case: $testcase"
+  echo 
   python 2.py "$testcase"
   echo "Finished running 2.py with test case: $testcase"
   echo "-------------------------"
+  echo
+  echo
 done
